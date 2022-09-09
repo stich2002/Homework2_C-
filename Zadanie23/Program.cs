2079e50b9a3dd7bee43b18ff0d154fce27b7c29d
Elena <elena15112002@gmail.com>
2022-09-09 18:47:00 +0500

    save23

---
+++ Zadanie23/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void SquareKub()
+{
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] answer = new int[N];
+if (N > 0)
+{
+for (int i = 1; i < (N + 1); i++)
+{
+answer[i - 1] = (i * i * i);
+}
+}
+else
+{
+Console.Write("Надо было вводить положительные числа");
+}
+var str = string.Join(" ", answer);
+Console.WriteLine(str);
+}
+
+try
+{
+SquareKub();
+}
+catch
+{
+Console.Write("Надо было вводить числа");
+}
